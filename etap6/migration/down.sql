a08eebe1b0634c7810e338337acdaa4aac0ab153
Mariusz Rychlicki <mariuszrychlicki04@gmail.com>
2025-11-26 08:05:40 +0100

    part 6: mariusz

--- etap6/migration/down.sql
+++ etap6/migration/down.sql
@@ -1,2 +1,6 @@
 DROP INDEX IF EXISTS idx_order_customer_id;
 DROP INDEX IF EXISTS idx_customer_user_id;
+DROP INDEX IF EXISTS idx_order_item_fulfillment_cook_id;
+DROP INDEX IF EXISTS idx_order_item_fulfillment_status_id;
+DROP INDEX IF EXISTS idx_order_item_delivery_courier_id;
+DROP INDEX IF EXISTS idx_order_item_delivery_status_id;
--- etap6/migration/up.sql
+++ etap6/migration/up.sql
@@ -1,6 +1,21 @@
-    CREATE INDEX IF NOT EXISTS idx_order_customer_id 
+    -- Tomek
+
+    CREATE INDEX IF NOT EXISTS idx_order_customer_id
         ON "order"(customer_id);
 
-    CREATE INDEX IF NOT EXISTS idx_customer_user_id 
+    CREATE INDEX IF NOT EXISTS idx_customer_user_id
         ON customer(user_id);
-        
+
+    -- Mariusz
+
+    CREATE INDEX IF NOT EXISTS idx_order_item_fulfillment_cook_id
+        ON order_item_fulfillment(cook_id);
+
+    CREATE INDEX IF NOT EXISTS idx_order_item_fulfillment_status_id
+        ON order_item_fulfillment(status_id);
+
+    CREATE INDEX IF NOT EXISTS idx_order_item_delivery_courier_id
+        ON order_item_delivery(courier_id);
+
+    CREATE INDEX IF NOT EXISTS idx_order_item_delivery_status_id
+        ON order_item_delivery(status_id);
