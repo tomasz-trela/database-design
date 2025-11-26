9c81c6722354ea5cc94d8ea6eba403b5da095bfc
Koziolek <koziolola43@gmail.com>
2025-11-26 11:18:53 +0100

    feat: part 6 Ola

--- etap6/migration/up.sql
+++ etap6/migration/up.sql
@@ -19,3 +19,40 @@
 
     CREATE INDEX IF NOT EXISTS idx_order_item_delivery_status_id
         ON order_item_delivery(status_id);
+
+    -- Ola
+    CREATE INDEX IF NOT EXISTS idx_complaint_customer_id
+        ON complaint(customer_id);
+        
+    CREATE INDEX IF NOT EXISTS idx_complaint_course_in_order_id
+        ON complaint(course_in_order_id);
+
+    CREATE INDEX IF NOT EXISTS idx_complaint_status
+        ON complaint(status);
+
+    CREATE INDEX IF NOT EXISTS idx_meal_plan_dietician_id
+        ON meal_plan(dietician_id);
+
+    CREATE INDEX IF NOT EXISTS idx_meal_plan_day_meal_plan_id
+        ON meal_plan_day(meal_plan_id);
+
+    CREATE INDEX IF NOT EXISTS idx_meal_plan_item_meal_plan_day_id
+        ON meal_plan_item(meal_plan_day_id);
+    
+    CREATE INDEX IF NOT EXISTS idx_meal_plan_item_course_id
+        ON meal_plan_item(course_id);
+
+    CREATE INDEX IF NOT EXISTS idx_daily_menu_dietician_id
+        ON daily_menu(dietician_id);
+
+    CREATE INDEX IF NOT EXISTS idx_daily_menu_item_menu_id
+        ON daily_menu_item(menu_id);
+    
+    CREATE INDEX IF NOT EXISTS idx_daily_menu_item_course_id
+        ON daily_menu_item(course_id);
+
+    CREATE INDEX IF NOT EXISTS idx_course_category_course_id
+        ON course_category(course_id);
+
+    CREATE INDEX IF NOT EXISTS idx_course_category_category_id
+        ON course_category(category_id);
